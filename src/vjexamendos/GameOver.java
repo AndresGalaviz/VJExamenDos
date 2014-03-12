768a7aaebaf1ca6c89b0d64904a3f1d369a3f45e
Alberto Astiazaran <pmdmaster@hotmail.com>
2014-03-12 13:03:37 -0600

    numeros y gameover

---
+++ src/vjexamendos/GameOver.java
@@ -0,0 +1,81 @@
+package vjexamendos;
+
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alberto
+ */
+public class GameOver implements MouseListener {
+    private final Boton PLAY, RETURN;
+    private final Image BACKGROUND;
+    
+    /**
+     * Metodo constructor
+     * @param background imagen de fondo
+     */
+    public GameOver(Image background) {
+        this.BACKGROUND = background;
+        PLAY = new Boton(Base.getW()/3, 4*Base.getH()/5, "Images/Buttons/play.png");
+        PLAY.setPosX(PLAY.getPosX() - PLAY.getAncho()/2);
+        PLAY.setPosY(PLAY.getPosY() - PLAY.getAlto()/2);
+        RETURN = new Boton(Base.getW()/2, 5*Base.getH()/6, "Images/Buttons/quit.png");
+        RETURN.setPosX(RETURN.getPosX() - RETURN.getAncho()/2);
+        RETURN.setPosY(RETURN.getPosY() - RETURN.getAlto()/2);
+    }
+    
+    public void actualiza () {
+        
+    }
+    
+    /**
+     * Dibuja la pantalla de seleccion de personajes
+     * @param g
+     * @param juego
+     */
+    public void render(Graphics g, Juego juego) {
+        
+        g.drawImage(BACKGROUND, 0, 0, juego);
+        g.drawImage(RETURN.getImagenI(), RETURN.getPosX(), RETURN.getPosY(), juego);
+        g.drawImage(PLAY.getImagenI(), PLAY.getPosX(), PLAY.getPosY(), juego);
+    }
+
+    /**
+     * Revisa clicks en los botones
+     * @param e 
+     */
+    @Override
+    public void mouseClicked (MouseEvent e) {
+        if (Juego.State == Juego.STATE.GAMEOVER) {
+            
+            if (RETURN.contiene (e.getX(), e.getY())) {
+                Juego.State = Juego.STATE.CHARSEL;
+            } if (PLAY.contiene (e.getX(), e.getY())) {
+                Juego.empezar = true;
+            }
+        
+        }
+        
+    }
+
+    @Override
+    public void mousePressed (MouseEvent e) {}
+
+    @Override
+    public void mouseReleased (MouseEvent e) {}
+
+    @Override
+    public void mouseEntered (MouseEvent e) {}
+
+    @Override
+    public void mouseExited (MouseEvent e) {}
+}
---
+++ src/vjexamendos/Number.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vjexamendos;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+
+/**
+ *
+ * @author Alberto
+ */
+public class Number {
+    private static final Image[] numbers = getNumbers();
+    /**
+     * Dibuja un numero
+     * @param g 
+     * @param juego
+     * @param N
+     * @param X
+     * @param Y 
+     */
+    public static void draw (Graphics g, Juego juego, int N, int X, int Y) {
+        int digits[] = new int[10];
+        int n = N, d = 0;
+        while (n > 0) {
+            digits[d++] = n%10;
+            n /= 10;
+        }
+        int totalLength = d*numbers[0].getWidth(juego) + (d-1)*10;
+        for (int i = 0; i < d; i++) {
+            int posX = X - totalLength/2 + i*numbers[d-i-1].getWidth(juego) + (i)*10;
+            int posY = Y - numbers[d-i-1].getWidth(juego)/2;
+            g.drawImage(numbers[digits[d-i-1]], posX, posY, juego);
+        }
+    }
+    
+    private static Image[] getNumbers() {
+        Image[] imgs = new Image[10];
+        for (int j = 0; j < 10; j++) {
+            imgs[j] = Toolkit.getDefaultToolkit().getImage(Number.class.getResource(j + ".png"));
+        }
+        return imgs;
+    }
+}
